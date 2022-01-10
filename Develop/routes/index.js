faa80374ca95be4c076862055fe6a26060d3c4bb
Smith1 <seth20smith@gmail.com>
2022-01-10 01:42:46 -0800

    final

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+
 router.use('/api', apiRoutes);
 
+
 router.use((req, res) => {
+  // check if sending message always
   res.send("<h1>Wrong Route!</h1>")
+  res.status(404).end();
 });
+
+
 
 module.exports = router;
